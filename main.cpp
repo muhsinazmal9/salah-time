63999b81b1b512689d22f54aa7945129ab49f036
Muhsin Azmal <muhsinstudent12@gmail.com>
2025-07-03 03:45:50 +0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Salah Time with GUI and alarm (will be use api = https://apps.bdwebs.com/salattimes/api/v2)";
+    return 0;
+}
