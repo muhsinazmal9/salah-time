e20a26835c4449926898fe068f11c0a6e82cebf4
muhsinazmal9 <themuhsindev@gmail.com>
2025-07-22 12:51:05 +0600

    Add basic X11 GUI window displaying "Salah Time 🕌"
    Ignore output directory in .gitignore.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include <X11/Xlib.h>
+#include <unistd.h>
 
 int main() {
-    std::cout << "Salah Time with GUI";
+    Display* d = XOpenDisplay(NULL);
+    if (d == NULL) return 1;
+
+    int s = DefaultScreen(d);
+    Window w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 300, 200, 1,
+                                   BlackPixel(d, s), WhitePixel(d, s));
+    XStoreName(d, w, "Salah Time 🕌");
+    XSelectInput(d, w, ExposureMask | KeyPressMask);
+    XMapWindow(d, w);
+
+    GC gc = DefaultGC(d, s);
+
+    while (true) {
+        XEvent e;
+        XNextEvent(d, &e);
+        if (e.type == Expose) {
+            XDrawString(d, w, gc, 50, 50, "Salah Time 🕌", 12);
+        } else if (e.type == KeyPress) {
+            break;
+        }
+    }
+
+    XCloseDisplay(d);
     return 0;
 }
